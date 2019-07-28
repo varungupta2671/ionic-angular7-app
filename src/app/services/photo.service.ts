86d61ca560dcf398a0177ecf1d277e49565c82a6
Varun Gupta <contact@itsvarungupta.com>
2019-07-28 14:47:06 +0530

    add gallery

---
+++ src/app/services/photo.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+// class Photo {
+//   data: any;
+// }
+
+export class PhotoService {
+
+  // public photos: Photo[] = [];
+  constructor() { }
+
+  // loadSaved() {
+  //   this.storage.get('photos').then((photos) => {
+  //     this.photos = photos || [];
+  //   });
+  // }
+}
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
 import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
-  currentImage: any;
 
-  constructor(private camera: Camera) { }
+// class Photo {
+//   data: any;
+// }
+
+export class Tab2Page {
+  // public photos: Photo[] = [];
+  public photos = [];
+  constructor(private camera: Camera, private storage: Storage) { }
   takePicture() {
     const options: CameraOptions = {
       quality: 100,
@@ -19,10 +25,26 @@ export class Tab2Page {
     }
 
     this.camera.getPicture(options).then((imageData) => {
-      this.currentImage = 'data:image/jpeg;base64,' + imageData;
+      // Add new photo to gallery
+      this.photos.unshift({
+        data: 'data:image/jpeg;base64,' + imageData
+      });
+
+      // Save all photos for later viewing
+      this.storage.set('photos', this.photos);
     }, (err) => {
-     // Handle error
-     console.log("Camera issue:" + err);
+      // Handle error
+      console.log("Camera issue: " + err);
     });
+  }
+
+  loadSaved() {
+    this.storage.get('photos').then((photos) => {
+      this.photos = photos || [];
+    });
+  }
+
+  ngOnInit() {
+    this.loadSaved();
   }
 }
